b77fa0a18dce16034e865d6cc64ef1041cc0c044
Tomáš Mikula <mail@tomasmikula.cz>
2021-10-17 13:48:40 +0200

    Update producent_consumer_semaphore.py

--- producent_consumer_semaphore.py
+++ producent_consumer_semaphore.py
@@ -6,8 +6,6 @@ from threading import Thread, Semaphore
 
 BUFFER_SIZE = random.randint(5, 10)
 
-buffer = [None] * BUFFER_SIZE
-
 
 def produce():
     item = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
@@ -21,21 +19,28 @@ def consume(item):
 
 
 def producent():
-    while True:
+    while loop_forever:
         time.sleep(random.randint(0, 4))
 
 
 def consumer():
-    while True:
+    while loop_forever:
         time.sleep(random.randint(0, 4))
 
     
+if __name__ == "__main__":
+    buffer = [None] * BUFFER_SIZE
+    loop_forever = True
 
-producent_thread = Thread(target=producent)
-consumer_thread = Thread(target=consumer)
+    producent_thread = Thread(target=producent)
+    consumer_thread = Thread(target=consumer)
 
-producent_thread.start()
-consumer_thread.start()
+    producent_thread.start()
+    consumer_thread.start()
 
-producent_thread.join()
-consumer_thread.join()
+    time.sleep(10)
+
+    loop_forever = False
+
+    producent_thread.join()
+    consumer_thread.join()
